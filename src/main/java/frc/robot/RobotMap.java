0f0a87ba8fccb5518494fb330b94037c021e61cf
Iwilleatdonuts <tjtang0310@gmail.com>
2025-01-05 22:02:34 -0500

    Initial Logged robot + Swerve drive

---
+++ src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,38 @@
+package frc.robot;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+
+public final class RobotMap {
+
+  public static final class SwerveConstants {
+
+    public static final class Mod0 {
+      public static final int driveMotorID = 0;
+      public static final int angleMotorID = 0;
+      public static final int canCoderID = 0;
+      public static final Rotation2d angleOffset = Rotation2d.fromDegrees(0);
+    }
+
+    public static final class Mod1 {
+      public static final int driveMotorID = 0;
+      public static final int angleMotorID = 0;
+      public static final int canCoderID = 0;
+      public static final Rotation2d angleOffset = Rotation2d.fromDegrees(0);
+    }
+
+    public static final class Mod2 {
+      public static final int driveMotorID = 0;
+      public static final int angleMotorID = 0;
+      public static final int canCoderID = 0;
+      public static final Rotation2d angleOffset = Rotation2d.fromDegrees(0);
+    }
+
+    public static final class Mod3 {
+      public static final int driveMotorID = 0;
+      public static final int angleMotorID = 0;
+      public static final int canCoderID = 0;
+      public static final Rotation2d angleOffset = Rotation2d.fromDegrees(0);
+    }
+
+  }
+}
